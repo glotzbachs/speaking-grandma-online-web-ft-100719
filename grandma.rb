f4bc2dfc2712ef4a695ffd48730802c3403ad6f6
Sarah Glotzbach <sarah.glotzbach@gmail.com>
2019-10-07 17:00:20 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,7 +15,7 @@
 def speak_to_grandma(phrase)
   
   if phrase=='I LOVE YOU GRANDMA!'
-    p "I LOVE  YOU TOO PUMPKIN!"
+    p "I LOVE YOU TOO PUMPKIN!"
   elsif phrase==phrase.upcase
     p "NO, NOT SINCE 1938!"
   else 
